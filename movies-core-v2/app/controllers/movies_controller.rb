70da98c707e23190b1d8d0380586039cd875af31
rafaelmatsumoto <rafaelmatsumoto43@gmail.com>
2019-11-11 20:53:17 -0300

    Core Service v2 - Initial commit
    Why:
    
    * Create main service
    
    This change addresses the need by:
    
    * #3

---
+++ movies-core-v2/app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+    before_action :set_movie, only: [:show]
+
+    def index
+        @movies = Movie.all
+        render json: @movies
+    end
+
+    def show
+        render json: @movie
+    end
+
+    private 
+
+    def set_movie
+        @movie = Movie.find(params[:id])
+    end
+end
