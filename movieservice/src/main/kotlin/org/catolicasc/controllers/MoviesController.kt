2da8cac613b5f0a7eb48c281bf800ac1fc11fc7b
rafaelmatsumoto <rafaelmatsumoto43@gmail.com>
2019-11-11 20:08:33 -0300

    General - Edit .gitignore file
    Why:
    
    This change addresses the need by:

---
+++ movieservice/src/main/kotlin/org/catolicasc/controllers/MoviesController.kt
@@ -0,0 +1,11 @@
+package org.catolicasc
+
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/movies")
+@Produces(MediaType.APPLICATION_JSON)
+class MoviesController {
+
+}
